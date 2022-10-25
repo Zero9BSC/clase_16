651d30de0132148ac0da5e45a97935e407cfe270
Zero9 <franconicolasjones@gmail.com>
2022-10-24 22:10:34 -0300

    Nuevo archivo 1

--- hola_mundo.py
+++ hola_mundo.py
@@ -4,5 +4,9 @@ print('Los de BTS se la re comen')
 
 print('Que putos que son')
 
+def suma(num1, num2):
+    resultado = num1 + num2
+    return resultado
 
+print(suma(5, 10))
 #ahora si papapaaaaaa ya dominas el push y el pull la puta madreeeeeee!!!!!!
