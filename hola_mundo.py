06e3ce63b4a85780bc29c56604cccaadc7a2f790
Zero9 <franconicolasjones@gmail.com>
2022-10-24 22:18:50 -0300

    Nuevo archivo 2

--- hola_mundo.py
+++ hola_mundo.py
@@ -8,5 +8,5 @@ def suma(num1, num2):
     resultado = num1 + num2
     return resultado
 
-print(suma(555, 410))
+print(suma(555, 10))
 #ahora si papapaaaaaa ya dominas el push y el pull la puta madreeeeeee!!!!!!
